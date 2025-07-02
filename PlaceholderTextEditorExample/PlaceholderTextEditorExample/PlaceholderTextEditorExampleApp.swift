8391dfe3019483b9a4dee375846646b59e989858
haruto318 <haruto.yhs318@gmail.com>
2025-07-02 14:11:00 +0900

    Exampleを追加

---
+++ PlaceholderTextEditorExample/PlaceholderTextEditorExample/PlaceholderTextEditorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceholderTextEditorExampleApp.swift
+//  PlaceholderTextEditorExample
+//
+//  Created by 濱野遥斗 on 2025/07/02.
+//
+
+import SwiftUI
+
+@main
+struct PlaceholderTextEditorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
